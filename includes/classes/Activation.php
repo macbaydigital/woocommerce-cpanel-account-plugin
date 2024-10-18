55deffaaaab81ae493dc5fc1f4806ecd9a483363
macbay <3510329+macbaydigital@users.noreply.github.com>
2024-10-18 21:00:41 +0200

    Create Activation.php
        Diese Klasse enthält den Code, der bei der Aktivierung des Plugins ausgeführt werden soll.
        Die activate-Methode ist eine statische Methode, die von WordPress aufgerufen wird, wenn das Plugin aktiviert wird.
        Derzeit ist die Methode leer, aber hier können Sie Code hinzufügen, um Dinge wie die Erstellung von Datenbanktabellen, das Hinzufügen von Standardeinstellungen oder andere Initialisierungsaufgaben durchzuführen.

---
+++ includes/classes/Activation.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Activation class
+ *
+ * @package WooCommerce_CPanel_Account_Plugin
+ */
+
+namespace WooCommerce_CPanel_Account_Plugin;
+
+/**
+ * Activation class
+ */
+class Activation
+{
+    /**
+     * Activate the plugin
+     */
+    public static function activate()
+    {
+        // Activation code goes here
+    }
+}

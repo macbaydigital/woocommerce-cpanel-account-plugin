1fbf1dc1642cbf3bd1bbae524c9fc4a45b16b6a2
macbay <3510329+macbaydigital@users.noreply.github.com>
2024-10-18 21:01:30 +0200

    Create Deactivation.php
        Diese Klasse enthält den Code, der bei der Deaktivierung des Plugins ausgeführt werden soll.
        Die deactivate-Methode ist eine statische Methode, die von WordPress aufgerufen wird, wenn das Plugin deaktiviert wird.
        Derzeit ist die Methode leer, aber hier können Sie Code hinzufügen, um Dinge wie das Löschen von Datenbanktabellen, das Entfernen von Optionen oder andere Aufräumarbeiten durchzuführen.

---
+++ includes/classes/Deactivation.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Deactivation class
+ *
+ * @package WooCommerce_CPanel_Account_Plugin
+ */
+
+namespace WooCommerce_CPanel_Account_Plugin;
+
+/**
+ * Deactivation class
+ */
+class Deactivation
+{
+    /**
+     * Deactivate the plugin
+     */
+    public static function deactivate()
+    {
+        // Deactivation code goes here
+    }
+}
